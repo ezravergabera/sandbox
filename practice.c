f9d32ee07fb1c97abafe1cc2980f66052a49a117
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-05-11 01:08:46 +0800

    tried createprocess()

--- practice.c
+++ practice.c
@@ -1,11 +1,50 @@
 #include <windows.h>
+#include <stdio.h>
 
+// int main(void) {
+//     MessageBoxW(
+//         NULL,
+//         L"Hello World!",
+//         L"My first messagebox!",
+//         MB_YESNOCANCEL | MB_ICONEXCLAMATION
+//     );
+//     return EXIT_SUCCESS;
+// }
+
+/*
+BOOL CreateProcessW(
+  [in, optional]      LPCWSTR               lpApplicationName,
+  [in, out, optional] LPWSTR                lpCommandLine,
+  [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,
+  [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,
+  [in]                BOOL                  bInheritHandles,
+  [in]                DWORD                 dwCreationFlags,
+  [in, optional]      LPVOID                lpEnvironment,
+  [in, optional]      LPCWSTR               lpCurrentDirectory,
+  [in]                LPSTARTUPINFOW        lpStartupInfo,
+  [out]               LPPROCESS_INFORMATION lpProcessInformation
+);
+*/
 int main(void) {
-    MessageBoxW(
+    STARTUPINFOW si = { 0 };
+    PROCESS_INFORMATION pi = { 0 };
+
+    if(!CreateProcessW(
+        L"C:\\Windows\\System32\\notepad.exe",
         NULL,
-        L"Hello World!",
-        L"My first messagebox!",
-        MB_YESNOCANCEL | MB_ICONEXCLAMATION
-    );
+        NULL,
+        NULL,
+        FALSE,
+        BELOW_NORMAL_PRIORITY_CLASS,
+        NULL,
+        NULL,
+        &si,
+        &pi)) {
+        
+        printf("(-) failed to create process, error: %ld", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    printf("(+) process started! pid: %ld", pi.dwProcessId);
     return EXIT_SUCCESS;
 }
