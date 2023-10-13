3cb384ad8642d756c6c621f2143305d3255c0b87
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-10-14 00:53:50 +0800

    read write asm basics

--- sandbox.asm
+++ sandbox.asm
@@ -3,16 +3,22 @@ global _start
 section .text
 
 _start:
-    MOV rax, 1
-    MOV rdi, 1
+    MOV rax, 0x0
+    MOV rdi, 0x0
     MOV rsi, msg
     MOV rdx, msglen
     SYSCALL
 
-    MOV rax, 60
-    MOV rdi, 0
+    MOV rax, 0x1
+    MOV rdi, 0x1
+    MOV rsi, msg
+    MOV rdx, msglen
+    SYSCALL
+
+    MOV rax, 0x3c
+    MOV rdi, 0x0
     SYSCALL
 
 section .data
-    msg: DB "Hello World", 0xA
-    msglen: EQU $ - msg
+    msg: DB "Test"
+    msglen: EQU 0x20
