f9ddcbd210f943a286343bb1a0c8f6ed29058cb0
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-04-11 17:17:12 +0800

    practiced hashing in nodejs

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,17 @@
-const { createHash } = require('crpyto');
+// hashing
+const { createHash } = require('crypto');
 
 function hash(input) {
-    return createHash('sha256');
+    return createHash('sha256').update(input).digest('hex');
 }
+
+let password = 'hi-mom';
+const hash1 = hash(password);
+console.log(hash1)
+
+password = 'hi-mom';
+const hash2 = hash(password);
+const match = hash1 === hash2;
+console.log(hash2);
+
+console.log(match ? 'good password' : 'password does not match');
